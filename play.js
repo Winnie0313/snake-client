88e94ad31f2415e817153d286108520653ba5b3c
Winnie Li <liwanyi0313@gmail.com>
2022-08-04 03:58:51 +0000

    pass the conn returnd by connect to setupInput to enable sending msg to server

--- play.js
+++ play.js
@@ -7,6 +7,7 @@ const { setupInput } = require("./input.js");
 console.log("Connecting ...");
 
 // call the connect funciton to connect to server
-connect();
-setupInput();
+const conn = connect();
+//connect();
+setupInput(conn);
 
