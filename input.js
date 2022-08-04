d5e1f6b98bdcebb422880a494a37f7b3a800f1d7
Winnie Li <liwanyi0313@gmail.com>
2022-08-04 03:57:08 +0000

    implemented the WASD movement to move the snake up, right, left or down

--- input.js
+++ input.js
@@ -1,7 +1,16 @@
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+
+// Stores the active TCP connection object.
+
+let connection;
+
+
+
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -17,6 +26,14 @@ const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
